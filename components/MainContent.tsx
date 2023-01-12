4f035c2cebfa7b8716413d483907fb7cb8edc025
czydomin <116489545+czydomin@users.noreply.github.com>
2023-01-12 20:59:29 +0100

    remove empty rows

---
+++ components/MainContent.tsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <div className="maincontent">
+      <img
+        src="https://media.licdn.com/dms/image/C4D03AQFzxWYqXgrANw/profile-displayphoto-shrink_800_800/0/1643227654385?e=2147483647&v=beta&t=9A3ida_jt4K86LVA_pbjZSFEDtikCmsFAqN0OtBZ07M"
+        className="header"
+      />
+      <h1>Dominika Czyszczonik</h1>
+      <h4>Frontend Developer</h4>
+      <p> dominika.czyszczonik.pl</p>
+      <button>Email</button>
+      <button>LinkedIn</button>
+    </div>
+  );
+}
+export default MainContent;
